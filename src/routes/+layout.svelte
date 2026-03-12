9416d9514835ad81e004f06489b7a03ef19b795a
Jonathan Steele <iNoles@users.noreply.github.com>
2026-03-12 18:33:43 -0400

    Import base path from $app/paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import './layout.css';
   import favicon from '$lib/assets/favicon.svg';
+  import { base } from '$app/paths';
 
   let { children } = $props();
   let mobileOpen = $state(false);
